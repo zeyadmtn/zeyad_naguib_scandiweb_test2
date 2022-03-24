41fe1de6badedbef6bfe2d13a547e41948a097da
Zeyad Naguib <82226885+zeyadmtn@users.noreply.github.com>
2022-03-24 16:41:40 +0200

    Initial commit

---
+++ magento/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/scandipwa/zeyad_naguib_scandiweb_test2',
+    __DIR__
+);
