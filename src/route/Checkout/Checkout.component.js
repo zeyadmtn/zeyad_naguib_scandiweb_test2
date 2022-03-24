f1ac2ce15adc15fecb4c019fa3f6b43388ba77be
Zeyad Naguib <82226885+zeyadmtn@users.noreply.github.com>
2022-03-24 19:52:38 +0200

    Update

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -1,49 +1,53 @@
-import {Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component'
-import ContentWrapper from 'Component/ContentWrapper';
-import './CheckoutComponent.style.scss';
+import ContentWrapper from "Component/ContentWrapper";
+import { Checkout as SourceCheckout } from "SourceRoute/Checkout/Checkout.component";
+import "./CheckoutComponent.style.scss";
+
 export class CheckoutComponent extends SourceCheckout {
+  renderProgressBar() {
+    let currentStep = this.props.checkoutStep;
+    return (
+      <div className="Checkout-progress-bar-wrapper">
+        <div className={"Checkout-progress-bar-line1-" + currentStep}></div>
+        <div>
+          <span class={"Checkout-progress-bar-milestone-text-m1-" + currentStep}>Shipping</span>
+          <div className={"Checkout-first-milestone-arrow-" + currentStep}>1</div>
+        </div>
 
 
-    renderProgressBar(){
-        return (
-            <div className='Checkout-progress-bar-wrapper'>
-                <div className='Checkout-progress-bar-line1'></div>
-                <div className='Checkout-first-milestone-arrow'>1</div>
-                <div className='Checkout-progress-bar-line2'></div>
-                <div className='Checkout-first-milestone-arrow'>2</div>
-                <div className='Checkout-progress-bar-line3'></div>
-            </div>
-        )
-    }
+        <div className={"Checkout-progress-bar-line2-" + currentStep}></div>
+        <div>
+          <span className={"Checkout-progress-bar-milestone-text-m2-" + currentStep}>Review & Payment</span>
+          <div className={"Checkout-second-milestone-arrow-" + currentStep}>2</div>
+        </div>
+        <div className={"Checkout-progress-bar-line3-" + currentStep}></div>
+      </div>
+    );
+  }
 
-    render() {
-        
-        return (
-            <main block="Checkout">
-                {this.renderProgressBar()}
-                <ContentWrapper
-                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
-                  label={ __('Checkout page') }
-                >
-                    
-                
-                    { this.renderSummary(true) }
-                    <div block="Checkout" elem="Step">
-                        { this.renderTitle() }
-                        { this.renderGuestForm() }
-                        { this.renderStep() }
-                        { this.renderLoader() }
-                    </div>
-                    <div>
-                        { this.renderSummary() }
-                        { this.renderPromo() }
-                        { this.renderCoupon() }
-                    </div>
-                </ContentWrapper>
-                
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main block="Checkout">
+        {this.renderProgressBar()}
+        <ContentWrapper
+          wrapperMix={{ block: "Checkout", elem: "Wrapper" }}
+          label={__("Checkout page")}
+        >
+          {this.renderSummary(true)}
+          <div block="Checkout" elem="Step">
+            {this.renderTitle()}
+            {this.renderGuestForm()}
+            {this.renderStep()}
+            {this.renderLoader()}
+          </div>
+          <div>
+            {this.renderSummary()}
+            {this.renderPromo()}
+            {this.renderCoupon()}
+          </div>
+        </ContentWrapper>
+      </main>
+    );
+  }
 }
 
 export default CheckoutComponent;
